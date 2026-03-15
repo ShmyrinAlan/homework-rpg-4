9ce190039d16f870ee844a2b7b8dbbfc65f801e1
Alan <alanshmyrinsource@gmail.com>
2026-03-15 23:30:27 +0500

    feat: add area and single target cast with same logic but different damage and targets

--- src/com/narxoz/rpg/bridge/AreaSkill.java
+++ src/com/narxoz/rpg/bridge/AreaSkill.java
@@ -12,5 +12,25 @@ public class AreaSkill extends Skill {
         // TODO: Area Bridge action
         // Apply resolved damage to a composite target.
         // Tip: Let Composite classes decide how to distribute AOE damage.
+
+        System.out.println(String.format("%s casts on %s",getSkillName(),target.getName()));
+        switch (getEffectName().toLowerCase()){
+            case "fire" -> {
+                if(target.getHealth() < 50) target.takeDamage(getBasePower() + 30);
+                else target.takeDamage(getBasePower() + 23);
+            }
+            case "ice" -> {
+                if(target.getHealth() > 50) target.takeDamage(getBasePower() + 28);
+                else target.takeDamage(getBasePower() + 25);
+            }
+            case "physical" -> {
+                if(target.getHealth() < 70) target.takeDamage(getBasePower() + 32);
+                else target.takeDamage(getBasePower() + 22);
+            }
+            case "shadow" -> {
+                if(target.getHealth() < 35) target.takeDamage(getBasePower() + 35);
+                else target.takeDamage(getBasePower() + 25);
+            }
+        }
     }
 }
--- src/com/narxoz/rpg/bridge/SingleTargetSkill.java
+++ src/com/narxoz/rpg/bridge/SingleTargetSkill.java
@@ -2,6 +2,8 @@ package com.narxoz.rpg.bridge;
 
 import com.narxoz.rpg.composite.CombatNode;
 
+import java.util.Collections;
+
 public class SingleTargetSkill extends Skill {
     public SingleTargetSkill(String skillName, int basePower, EffectImplementor effect) {
         super(skillName, basePower, effect);
@@ -12,5 +14,26 @@ public class SingleTargetSkill extends Skill {
         // TODO: Single-target Bridge action
         // 1) Resolve final damage through effect implementor
         // 2) Apply to target node
+        if(target.getChildren().isEmpty()){
+            System.out.println(String.format("%s casts on %s",getSkillName(),target.getName()));
+            switch (getEffectName().toLowerCase()){
+                case "fire" -> {
+                    if(target.getHealth() < 30) target.takeDamage(getBasePower() + 10);
+                    else target.takeDamage(getBasePower() + 3);
+                }
+                case "ice" -> {
+                    if(target.getHealth() > 30) target.takeDamage(getBasePower() + 8);
+                    else target.takeDamage(getBasePower() + 5);
+                }
+                case "physical" -> {
+                    if(target.getHealth() < 50) target.takeDamage(getBasePower() + 12);
+                    else target.takeDamage(getBasePower() + 2);
+                }
+                case "shadow" -> {
+                    if(target.getHealth() < 15) target.takeDamage(getBasePower() + 15);
+                    else target.takeDamage(getBasePower() + 5);
+                }
+            }
+        }else cast(target.getChildren().getFirst());
     }
 }
